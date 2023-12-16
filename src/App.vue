ad396561b9c9364efdd457043790bd2db8d63596
Daniil <dtuneev@gmail.com>
2023-12-16 11:44:00 +0200

    adding carousel to choose image template

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import FirstStep from './components/FirstStep.vue'
+import {ref} from "vue";
+
+const step = ref(1);
+
+const log = (e) => {
+  console.log('e');
+  console.log(e);
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="content">
+    <FirstStep v-if="step === 1" class="full" @chooseImg="log"/>
   </div>
-  <v-text-field
-      variant="outlined"
-  />
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  max-width: 80%;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/FirstStep.vue
@@ -0,0 +1,62 @@
+<script setup>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import {ref, getCurrentInstance} from "vue";
+
+const imgs = ref([
+  'same_picture.webp',
+  'same_picture.webp',
+  'same_picture.webp',
+  'drake.jpeg',
+  'head.jpeg',
+  'sup.jpeg',
+  'me_and_boys.png',
+]);
+
+const { emit } = getCurrentInstance();
+
+const chooseImg = (e) => {
+  emit('chooseImg', e.target.src);
+}
+</script>
+
+<template>
+  <carousel :items-to-show="1.5">
+    <slide v-for="img in imgs" :key="img" @click="chooseImg">
+      <img :src="'memes/' + img" :alt="img">
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+</template>
+
+<style scoped>
+.carousel__item {
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  max-height: 94vh;
+  padding: 10px;
+}
+
+.carousel__slide img {
+  max-width: 50vw;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
+</style>
